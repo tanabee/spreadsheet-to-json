e564922520d424d46ca43ef5aa59c94dea928534
tanabee <tanabee1986@gmail.com>
2018-09-03 22:53:14 +0900

    fix indent

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ var spreadSheetId = '';// スプレッドシート ID
 var spreadSheetTabName = '';// スプレッドシートのタブ名
 
 function doGet(e) {
-	var rows = SpreadsheetApp
+  var rows = SpreadsheetApp
     .openById(spreadSheetId)
     .getSheetByName(spreadSheetTabName)
     .getDataRange()
     .getValues();
-	var keys = rows.shift();
-	var data = rows.map(function(row) {
-		var obj = {};
-		row.forEach(function(element, i) {
-			obj[keys[i]] = element;
-		});
-		return obj;
-	});
-	return ContentService.createTextOutput(JSON.stringify(data))
-											 .setMimeType(ContentService.MimeType.JSON);
+  var keys = rows.shift();
+  var data = rows.map(function(row) {
+    var obj = {};
+    row.forEach(function(element, i) {
+      obj[keys[i]] = element;
+    });
+    return obj;
+  });
+  return ContentService.createTextOutput(JSON.stringify(data))
+                       .setMimeType(ContentService.MimeType.JSON);
 }
