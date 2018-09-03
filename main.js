2c1c674dca195953a68ffe128bd06045ab610209
tanabee <tanabee1986@gmail.com>
2018-09-03 22:50:53 +0900

    First Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var spreadSheetId = '';// スプレッドシート ID
+var spreadSheetTabName = '';// スプレッドシートのタブ名
+
+function doGet(e) {
+	var rows = SpreadsheetApp
+    .openById(spreadSheetId)
+    .getSheetByName(spreadSheetTabName)
+    .getDataRange()
+    .getValues();
+	var keys = rows.shift();
+	var data = rows.map(function(row) {
+		var obj = {};
+		row.forEach(function(element, i) {
+			obj[keys[i]] = element;
+		});
+		return obj;
+	});
+	return ContentService.createTextOutput(JSON.stringify(data))
+											 .setMimeType(ContentService.MimeType.JSON);
+}
